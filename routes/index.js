80fb5e741527891f7ae1389c993b5d9d14214824
JH <jalal@superusers.dk>
2016-06-01 12:06:25 +0200

    ok

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
+// (community) npm install express
 var express = require('express');
 var router = express.Router();
+// NODE API module
+//var request = require('request')
+
+// ? hvad skal jeg huske her ?
+// COMMUNITY MODULE
+var request  	= require( "request-promise" )
+
+//system module node API 
+var path = require('path')
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  
+  //var data = require(path.join(__dirname,'../','db' ,'data.json'))
+    
+    request('http://couchdb.itacademy.dk/bogi/2016' )
+    .then(function(data){
+	  	res.render('index', JSON.parse( data ).products );
+	  })
+  
 });
 
 module.exports = router;
